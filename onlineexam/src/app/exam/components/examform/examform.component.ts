84a906d1ef455504a4bc07c795aa36fef9666f2d
gopika <gopikalakshmia@gmail.com>
2022-02-08 12:40:00 -0700

    added multiple questions and dynamic controls

--- onlineexam/src/app/exam/components/examform/examform.component.ts
+++ onlineexam/src/app/exam/components/examform/examform.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-examform',
@@ -34,17 +34,53 @@ export class ExamformComponent implements OnInit {
     "tags": [],
     "category": "linux",
     "difficulty": "Easy"
-  }
+  },
+  {
+    "id": 2,
+    "question": "How to delete  in Linux?",
+    "description": "delete folder",
+    "answers": {
+      "answer_a": "ls",
+      "answer_b": "delete",
+      "answer_c": "remove",
+      "answer_d": "rmdir",
+      "answer_e": null,
+      "answer_f": null
+    },
+    "multiple_correct_answers": "false",
+    "correct_answers": {
+      "answer_a_correct": "false",
+      "answer_b_correct": "false",
+      "answer_c_correct": "false",
+      "answer_d_correct": "true",
+      "answer_e_correct": "false",
+      "answer_f_correct": "false"
+    },
+    "explanation": "rmdir deletes an empty directory",
+    "tip": null,
+    "tags": [],
+    "category": "linux",
+    "difficulty": "Easy"
+  },
+  
 ]
   questionform!: FormGroup;
 
-  constructor() { }
+  constructor( public form:FormBuilder) {
+    this.questionform=new FormGroup({});
+   }
 
   ngOnInit(): void {
-    this.questionform=new FormGroup({
-      answers:new FormControl('')
-      
-    })
+
+
+  this.Questions.forEach(q=>{
+  if(this.questionform!=undefined){
+    console.log(q.question);
+    this.questionform.addControl(q.question.toString(),new FormControl(""));
+  }
+}
+  )
+
   }
   submit(){
     console.log(this.questionform.value);
