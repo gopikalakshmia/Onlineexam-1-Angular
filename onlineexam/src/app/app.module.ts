098eb136ee4043164bb290ea9990142ddd3216b7
gopika <gopikalakshmia@gmail.com>
2022-02-10 20:21:25 -0700

    score card added

--- onlineexam/src/app/app.module.ts
+++ onlineexam/src/app/app.module.ts
@@ -1,17 +1,20 @@
+import { HomeModule } from './home/home.module';
+import { StartComponent } from './home/components/start/start.component';
 import { ExamformComponent } from './exam/components/examform/examform.component';
 import { ScoreComponent } from './score/components/score/score.component';
 import { RouterModule } from '@angular/router';
 import { ScoreModule } from './score/score.module';
 import { ExamModule } from './exam/exam.module';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http'
 const routes=[
-  {path:'',component:ExamformComponent},
-  {path:'score',component:ScoreComponent}
+  {path:'',component:StartComponent},
+  {path:'score',component:ScoreComponent},
+  {path:'examform',component:ExamformComponent}
 ]
 
 @NgModule({
@@ -24,6 +27,7 @@ const routes=[
     ExamModule,
     HttpClientModule,
     ScoreModule,
+    HomeModule,
     RouterModule.forRoot(routes)
   ],
   providers: [],
